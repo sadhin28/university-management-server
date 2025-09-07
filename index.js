e1ed82a504a998ac9eef377a0e9df671a4f6e5aa
sadhin28 <taosifsadhin113@gmail.com>
2025-09-07 12:05:12 +0600

    coment the roigin

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ require('dotenv').config()
 
 app.use(express.json({ limit: "500mb" }));
 
-app.use( cors({
-    origin:  "https://university-management-sy-dc929.web.app", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  }));
+app.use( cors());
 
-
+// {
+//     origin:  "https://university-management-sy-dc929.web.app", 
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   }
 
 // Firebase Admin Initialize
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
